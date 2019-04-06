13fc68a612e1c8c3517730b327785bfbabf6e29f
suryakalyanr <suryakalyan.rani@gmail.com>
2019-04-06 15:16:37 -0600

    Initial Version

---
+++ src/main/java/org/srivalli/simple_java_project/App.java
@@ -0,0 +1,13 @@
+package org.srivalli.simple_java_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
